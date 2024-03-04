c1a8ca4ddbd8ddd9cf4cd2657be489e869af2696
Matheus Vaccaro <matheusvaccaro@gmail.com>
2024-03-04 17:33:28 -0300

    Add Rabble Wabble initial project

---
+++ 03-model-view-controller-pattern/Rabble Wabble/Rabble Wabble/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rabble Wabble
+//
+//  Created by Matheus Vaccaro on 04/03/24.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
